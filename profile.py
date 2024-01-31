a42387c0df71b0b5f87ade681727a38b5b0803b7
Yue Tan <yuetan@cs.princeton.edu>
2024-01-31 16:26:40 -0500

    first attempt

--- profile.py
+++ profile.py
@@ -1,27 +1,54 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
-
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
-
-Instructions:
-Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
+"""A cluster of r7525 nodes, attached to a LAN
 """
 
-# Import the Portal object.
 import geni.portal as portal
-# Import the ProtoGENI library.
-import geni.rspec.pg as pg
+import geni.rspec.pg as rspec
 
-# Create a portal context.
+imageList = [
+    ('urn:publicid:IDN+clemson.cloudlab.us+image+praxis-PG0:ubuntu2204-doca2.2.0', 'UBUNTU 22.04 DOCA 2.2.0'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD', 'UBUNTU 22.04'),
+]
+
 pc = portal.Context()
+pc.defineParameter("num_nodes", "Number of GPU nodes",
+                   portal.ParameterType.INTEGER, 1)
+pc.defineParameter("os_image", "OS image",
+                   portal.ParameterType.IMAGE, imageList[0], imageList)
+pc.defineParameter("data_size", "local storage size",
+                   portal.ParameterType.STRING, "512GB")
+params = pc.bindParameters()
 
-# Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.RawPC("node")
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+# add lan
+lan = request.LAN("Lan")
+lan.best_effort = True
+lan.vlan_tagging = True
+lan.link_multiplexing = True
 
-# Print the RSpec to the enclosing page.
+# add bluefield for r7525 hw type
+global linkbf
+linkbf = request.Link('bluefield')
+linkbf.type = "generic_100g"
+
+# normal nodes
+for i in range(params.num_nodes):
+    node = request.RawPC("node{}".format(i + 1))
+    node.disk_image = params.os_image
+    node.hardware_type = 'r7525'
+    bs = node.Blockstore("bs{}".format(i + 1), "/data")
+    bs.size = params.data_size
+    intf = node.addInterface("if1")
+    # r7525 requires special config to use its normal 25Gbps experimental network
+    intf.bandwidth = 25600
+    # Initialize BlueField DPU.
+    bfif = node.addInterface("bf")
+    bfif.addAddress(rspec.IPv4Address(
+        "192.168.10.{}".format(i + 1), "255.255.255.0"))
+    bfif.bandwidth = 100000000
+    linkbf.addInterface(bfif)
+    intf.addAddress(rspec.IPv4Address(
+        "192.168.1.{}".format(i + 1), "255.255.255.0"))
+    lan.addInterface(intf)
+
 pc.printRequestRSpec(request)
